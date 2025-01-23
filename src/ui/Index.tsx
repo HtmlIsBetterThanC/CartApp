f0a4b53bade442b4de168db3ff7fb5f867b52654
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2025-01-23 12:42:05 +0100

    wip: added react-navigation

--- src/ui/Index.tsx
+++ src/ui/Index.tsx
@@ -1,7 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import { View } from 'react-native';
 
-const Index = () => {
+enum Routes {
+  products = 'Products',
+}
+
+type RootStackParamList = {
+  Products: undefined;
+};
+
+type ProductsProps = NativeStackScreenProps<RootStackParamList, Routes.products>;
+
+function RouteProducts({ navigation }: ProductsProps) {
   return <View></View>;
+}
+
+const Index = () => {
+  const Stack = createNativeStackNavigator<RootStackParamList>();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={Routes.products} component={RouteProducts}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Index;

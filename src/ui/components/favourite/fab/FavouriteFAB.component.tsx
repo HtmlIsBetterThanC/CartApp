df1d7c3430866eac69b6e41c8c919400e243f8fa
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2025-01-24 14:55:04 +0100

    feat: added FavouriteFAB

---
+++ src/ui/components/favourite/fab/FavouriteFAB.component.tsx
@@ -0,0 +1,27 @@
+import { StyleProp, ViewStyle } from 'react-native';
+import { FAB } from 'react-native-paper';
+import { styles } from '@/src/ui/components/favourite/fab/FavouriteFAB.styles';
+import { MD3Theme } from 'react-native-paper/lib/typescript/types';
+
+interface FavouriteFABProps {
+  theme: MD3Theme;
+  isFavourite: boolean;
+  onPress: () => void;
+  containerStyle?: StyleProp<ViewStyle>;
+}
+
+const FavouriteFAB = (props: FavouriteFABProps) => {
+  return (
+    <FAB
+      style={[styles.fab, props.containerStyle]}
+      theme={props.theme}
+      elevation={4}
+      size={'medium'}
+      icon={props.isFavourite ? 'heart' : 'heart-outline'}
+      color={props.isFavourite ? props.theme.colors.onPrimaryContainer : 'grey'}
+      onPress={props.onPress}
+    />
+  );
+};
+
+export default FavouriteFAB;

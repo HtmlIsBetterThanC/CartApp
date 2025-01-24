096db9907aa2fde11b61b64421e00b245b55e5f0
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2025-01-24 20:30:42 +0100

    fix: fixed Product screen not filling all the space available

--- src/ui/screens/product/Product.screen.tsx
+++ src/ui/screens/product/Product.screen.tsx
@@ -1,10 +1,9 @@
-import { Animated, StyleProp, ViewStyle } from 'react-native';
+import { ScrollView, StyleProp, useWindowDimensions, ViewStyle } from 'react-native';
 import UiProduct from '@/src/model/ui/UiProduct';
 import { ProgressBar, Surface, useTheme } from 'react-native-paper';
 import { styles } from '@/src/ui/screens/product/Product.styles';
 import ProductDetail from '@/src/ui/components/product/detail/ProductDetail.component';
 import FavouriteFAB from '@/src/ui/components/favourite/fab/FavouriteFAB.component';
-import ScrollView = Animated.ScrollView;
 
 interface ProductProps {
   isLoading: boolean;
@@ -15,6 +14,7 @@ interface ProductProps {
 
 const Product = (props: ProductProps) => {
   const theme = useTheme();
+  const minHeight = useWindowDimensions().height - 150;
   const product = props.product;
 
   return (
@@ -23,7 +23,7 @@ const Product = (props: ProductProps) => {
         {props.isLoading || !props.product ? (
           <ProgressBar indeterminate={true} />
         ) : (
-          <ProductDetail product={product!} />
+          <ProductDetail containerStyle={{ minHeight: minHeight }} product={product!} />
         )}
       </Surface>
       <FavouriteFAB

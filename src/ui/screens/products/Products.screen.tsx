297827387ecda7b59f4dc04297515c01b6636049
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2025-01-24 15:39:35 +0100

    feat: added filter callbacks to Products screen

--- src/ui/screens/products/Products.screen.tsx
+++ src/ui/screens/products/Products.screen.tsx
@@ -3,20 +3,43 @@ import { StyleProp, ViewStyle } from 'react-native';
 import { Surface, useTheme } from 'react-native-paper';
 import { styles } from '@/src/ui/screens/products/Products.styles';
 import ProductList from '@/src/ui/components/product/list/ProductList.component';
+import UiCategory from '@/src/model/ui/UiCategory';
+import FilterButtons from '@/src/ui/components/filter/FilterButtons.component';
+import { useCallback, useState } from 'react';
 
 interface ProductProps {
   isLoading: boolean;
   products: UiProduct[];
   onProductPress: (id: number) => void;
   onFavouritePress: (product: UiProduct) => void;
+  onCategoryPress: (category: UiCategory) => void;
+  onRatingPress: () => void;
   containerStyle?: StyleProp<ViewStyle>;
 }
 
 const Products = (props: ProductProps) => {
   const theme = useTheme();
+  const [selectedCategory, setSelectedCategory] = useState<UiCategory>();
+  const [isRatingAscending, setRatingAscending] = useState(true);
+
+  const onCategoryPress = () => {
+    selectedCategory && props.onCategoryPress(selectedCategory);
+  };
+
+  const onRatingPress = useCallback(() => {
+    setRatingAscending((prev) => !prev);
+    props.onRatingPress();
+  }, [props]);
 
   return (
     <Surface style={[styles.container, props.containerStyle]}>
+      <FilterButtons
+        theme={theme}
+        selectedCategory={selectedCategory}
+        isRatingAscending={isRatingAscending}
+        onCategoryPress={onCategoryPress}
+        onRatingPress={onRatingPress}
+      />
       <ProductList
         theme={theme}
         isLoading={props.isLoading}
